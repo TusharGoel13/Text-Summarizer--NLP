dc526c5e1db3274e88a886e8cff264feaeaded8e
Tushar Goel <tushargoel4613@gmail.com>
2024-01-21 03:32:55 +0530

    Data Validation Stage Completed

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -17,3 +17,9 @@ class DataIngestionConfig:
     source_URL: str  # URL from which data will be fetched
     local_data_file: Path  # Local path where the data file will be saved
     unzip_dir: Path  # Directory where the data file will be extracted
+    
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
