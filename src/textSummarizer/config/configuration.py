1ddaebdca8fa98a927cfcd2dae377a39fe5ba79b
Tushar Goel <tushargoel4613@gmail.com>
2024-01-21 03:52:42 +0530

    Data Transformation Stage Completed

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -3,6 +3,7 @@ from textSummarizer.utils.common import read_yaml, create_directories
 
 from textSummarizer.entity import DataIngestionConfig
 from textSummarizer.entity import DataValidationConfig
+from textSummarizer.entity import DataTransformationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -58,3 +59,17 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+            tokenizer_name = config.tokenizer_name
+        )
+
+        return data_transformation_config
